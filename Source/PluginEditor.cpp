542b214b4a3a3d0f9020c5d12eac12ed191414b9
jordan-mcwilliams <jsmcwilliams@gmail.com>
2024-01-01 19:46:45 -0800

    Started working on Frequency Analyzer for the GUI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -34,12 +34,80 @@ SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 //==============================================================================
 void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (juce::Colours::black);
+    
+    auto bounds = getLocalBounds();
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+    auto w = responseArea.getWidth();
+    auto& lowcut = monoChain.get<ChainPositions::LowCut>();
+    auto& peak = monoChain.get<ChainPositions::Peak>();
+    auto& highcut = monoChain.get<ChainPositions::HighCut>();
+    
+    auto sampleRate = audioProcessor.getSampleRate();
+    
+    // Computing 1 magnitude per pixel
+    std::vector<double> mags;
+    mags.resize(w);
+    
+    // Need to iterate through each pixel and compute magnitude at that frequency
+    // Magnitude is expressed in Gain units, which are multiplicative, so we start at 1
+    for (int i = 0; i < w; ++i)
+    {
+        double mag = 1.f;
+        // Need to call magnitude function for particular pixel mapped from pixel space to freq space
+        // helper function mapToLog10 maps the normalized pixel number to its freq within human hearing range
+        auto freq = juce::mapToLog10((double(i)) / double(w), 20.0, 20000.0);
+        
+        if (!monoChain.isBypassed<ChainPositions::Peak>())
+            mag *= peak.coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        
+        if (!lowcut.isBypassed<0>())
+            mag *= lowcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!lowcut.isBypassed<1>())
+            mag *= lowcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!lowcut.isBypassed<2>())
+            mag *= lowcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!lowcut.isBypassed<3>())
+            mag *= lowcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        
+        if (!highcut.isBypassed<0>())
+            mag *= highcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!highcut.isBypassed<1>())
+            mag *= highcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!highcut.isBypassed<2>())
+            mag *= highcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if (!highcut.isBypassed<3>())
+            mag *= highcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        
+        mags[i] = juce::Decibels::gainToDecibels(mag);
+    }
+    
+    juce::Path responseCurve;
+    
+    // Mapping decibel values to response area with helper lambda
+    const double outputMin = responseArea.getBottom();
+    const double outputMax = responseArea.getY();
+    auto map = [outputMin, outputMax](double input)
+    {
+        // Maps decibels to screen coordinates
+        return juce::jmap(input, -24.0, 24.0, outputMin, outputMax);
+    };
+    
+    // Now we can start new subpath with first magnitude
+    responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
+    
+    for (size_t i = 1; i < mags.size(); ++i)
+    {
+        responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
+    }
+    
+    // Now we make a background border and draw the path
+    g.setColour(juce::Colours::orange);
+    g.drawRoundedRectangle(responseArea.toFloat(), 4.f, 1.f);
+    
+    g.setColour(juce::Colours::white);
+    g.strokePath(responseCurve, juce::PathStrokeType(2.f));
+    
 }
 
 void SimpleEQAudioProcessorEditor::resized()
